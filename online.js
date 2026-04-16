7c9f84ad7c34058460d4f7c0d8bc79650657dd45
lithopskinberry-jpg <lithops.kinberry@gmail.com>
2026-04-17 01:01:55 +0900

    Update online.js

--- online.js
+++ online.js
@@ -16,7 +16,6 @@ const firebaseConfig = {
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-</script>
   authDomain: "YOUR_AUTH_DOMAIN",
   databaseURL: "YOUR_DATABASE_URL",
   projectId: "YOUR_PROJECT_ID",
